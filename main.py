2444d584ecd1733f7737912753d8c9c4355e6aa2
TravUCD2021 <adamt757@gmail.com>
2021-09-22 15:40:08 +0100

    Updated One month files

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+data=pd.read_csv("Month.csv")
+
+print(data)
+
+y=data["date_time"]
+x=data["amount"]
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+
+ax.set(xlabel='Value', ylabel='Dates',
+       title='USD Payments')
+ax.grid()
+
+
+plt.show()
